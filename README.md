b477f46a35ef38d7b39c76a039a94ddac6935df2
HisAtri <yz@ghacg.com>
2023-12-05 18:12:00 +0800

    update参数指定版本

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ Ubuntu Shell toolset
 ```bash
 bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/HisAtri/Ubuntools@master/HelloWorld/helloworld.sh)"
 ```
+
+## Python
+
+### Update
+
+example: install python3.11
+
+```bash
+sudo bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/HisAtri/Ubuntools@dev/python/update.sh) 3.11"
+```
--- python/update.sh
+++ python/update.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# 添加Python3.11仓库
+# 添加仓库
 add-apt-repository ppa:deadsnakes/ppa -y
 apt update
+version="$1"
 
-# 安装Python3.11
-apt install -y python3.11
+# 安装Python对应版本
+if apt install -y python$version; then
+    echo "Python $version 已成功安装。"
+else
+    echo "无法找到 Python $version 版本。请检查版本号并确保仓库中存在该版本。"
+    exit 1
+fi
 
-# 获取系统上安装的Python版本
+# 获取原有的Python版本
 installed_python_version=$(python3 --version 2>&1 | awk '{print $2}')
 
 # 更新Python3的符号链接（使用系统上已安装的最新版本）
@@ -15,19 +21,17 @@ update-alternatives --install /usr/bin/python3 python3 /usr/bin/$installed_pytho
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2
 
 # 更新pip
-python3.11 -m ensurepip
-python3.11 -m pip install --upgrade pip
+python$version -m ensurepip
+python$version -m pip install --upgrade pip
 
-# 安装aptitude以便处理可能的依赖关系
 apt install -y aptitude
-
-# 获取所有依赖于旧版Python的软件包
+# 获取所有依赖于旧版Python的包
 dependents=$(aptitude search '?reverse-depends(^python3-*, ^python3.*$)' -F '%p')
-
-# 安装所有依赖于旧版Python的软件包
+# 安装
 apt install -y $dependents
 
 # 清理APT缓存
 apt clean
 
-echo "Python 3.11 已成功安装并配置。"
+echo "Python $version 已成功安装并配置。"
+echo "此源代码发布于GitHub：https://github.com/HisAtri/Ubuntools"
